91be502da7d759026db4edd0acb84ab5cc9f98f1
InksterZachary <z.j.inkster@gmail.com>
2020-11-12 10:11:39 -0600

    Added initial classes and added member variable(s) to Moves.cs

---
+++ Moves.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+    class Moves
+    {
+        public string defeats;
+        public List<Moves> attackMethod;
+
+        public Moves(string defeats)
+        {
+            this.defeats = defeats;
+            attackMethod = new List<Moves>();
+        }
+
+        public bool Paper(Moves move)
+        {
+            
+        }
+    }
+}
